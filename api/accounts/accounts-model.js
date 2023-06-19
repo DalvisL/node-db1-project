20ebb4e42df1201b4a6e753905c1d923b6db41c4
Dalvis <Dalvis.liz27@gmail.com>
2023-06-19 15:14:02 -0400

    added all queries to the accounts model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -7,18 +7,28 @@ const getAll = async () => {
 
 const getById = async id => {
   // DO YOUR MAGIC
+  return await db('accounts').where('id', id).first();
 }
 
 const create = async account => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert(account);
+  const newAccount = await getById(id);
+  return newAccount;
 }
 
 const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  await db('accounts').where('id', id).update(account);
+  const updatedAccount = await getById(id);
+  return updatedAccount;
 }
 
 const deleteById = async id => {
   // DO YOUR MAGIC
+  const toBeDeleted = await getById(id);
+  await db('accounts').where('id', id).del();
+  return toBeDeleted;
 }
 
 module.exports = {
